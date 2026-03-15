947dd85c49a7e936e4d85c55f3e976dd1a2bfb5e
Florencia Vivar <daianavivar93@gmail.com>
2026-03-15 19:36:27 +0100

    fix the NotFound page

--- src/Notfound.tsx
+++ src/Notfound.tsx
@@ -1,8 +1,15 @@
+import {Link} from "react-router-dom";
+
 export function NotFound() {
     return (
         <div className="min-h-screen flex items-center justify-center">
             <h1 className="text-3xl font-bold">404 - Página no encontrada</h1>
-            <a href="/" className="text-primary mt-4 inline-block">← Volver al inicio</a>
+            <Link
+                to="/"
+                className="text-primary mt-4 inline-block"
+            >
+                ← Volver al inicio
+            </Link>
         </div>
     );
 }
